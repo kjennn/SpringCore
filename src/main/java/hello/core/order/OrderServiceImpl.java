938b722960842a0a443134ee76c015e2aa37e95c
kjennn <gva05037@gmail.com>
2022-02-19 18:21:06 +0900

    DIP 를 지키려면? 관심사를 분리해야한다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixedDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemmoryMemberRepository;
@@ -9,7 +10,11 @@ import hello.core.member.MemmoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemmoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+  //  private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+  //  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //DIP 를 지키려면? - 관심사의 분리
+      private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memerId, String itemName, int itemPrice) {
