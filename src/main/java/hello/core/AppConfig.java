d298759c628199048657e95e7712f62badeb24ed
kjennn <gva05037@gmail.com>
2022-03-01 17:23:26 +0900

    AppConfig을 스프링으로 전환
    - @Configuration, @Bean을 이용하여 객체들을 스프링 컨테이너에 등록,
    - 이 객체들은 스프링 빈이라고 부름.
    -  스프링 컨테이너에 넣어서 관리하면 엄청난 장점들이 있다........

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,25 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemmoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    // AppConfig는 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성" 한다.
+    // AppConfig는 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성" 한다. 구성정보, 설정정보....
     // 생성한 객체 인스턴스의 참조(래퍼런스)를 "생성자를 통해서 주입(연결), 인젝션" 해준다.
     // MemberServiceImpldms MemoryMemberRepository 를 의존하지 않는다.
     // 의존관계에 대한 고민은 외부에 맡기고 실행에만 집중할 수 있다.    => DIP 완성, 관심사 확실히 분리
 
+    @Bean // 스프링 컨테이너에 등록함
     public MemberService memberService(){
-        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입 , 객체를 스프링 컨테이너에 등록, 스프링 빈이라고 함.
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy()); // 생성자 주입
     }
 
-    private MemmoryMemberRepository memberRepository() { // 역할이 드러나게 리팩토링 - 변경시 이 부분만 변경하면 됨
+    @Bean
+    public MemmoryMemberRepository memberRepository() { // 역할이 드러나게 리팩토링 - 변경시 이 부분만 변경하면 됨
         return new MemmoryMemberRepository();
     }
+
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixedDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class); //스프링컨테이너에 집어넣어 관리
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 이름, 타입.... 이름은 appconfig의 메소드명
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
